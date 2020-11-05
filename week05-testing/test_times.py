5ef2b6e9d4bfd47511b13aec3ff624e567faa64e
Lesley <lesley.gover.20@ucl.ac.uk>
2020-11-05 11:49:05 +0000

    added more tests

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,6 +1,7 @@
 # import the functions made in times
 from times import compute_overlap_time
 from times import time_range
+from pytest import raises
 
 # function to test
 def test_given_input():
@@ -10,3 +11,28 @@ def test_given_input():
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     
     assert result == expected
+
+def test_no_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 13:30:00", "2010-01-12 13:45:00", 2, 60)
+    expected = []
+    
+    assert (compute_overlap_time(large, short)) == expected
+
+def test_both_intervals():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00",2,0)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 11:00:00", 2, 0)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:45:00'), ('2010-01-12 10:45:00', '2010-01-12 11:00:00')]
+    
+    assert (compute_overlap_time(large, short)) == expected
+
+def test_times_touching():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 11:00:00",2,0)
+    short = time_range("2010-01-12 11:00:00", "2010-01-12 12:00:00", 2, 0)
+    expected = []
+    
+    assert (compute_overlap_time(large, short)) == expected
+
+def test_time_check():
+    with raises(ValueError):
+        time_range("2010-01-12 11:00:00","2010-01-12 10:00:00")

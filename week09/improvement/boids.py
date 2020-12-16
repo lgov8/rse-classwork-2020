8ab84c96ed3377c2b57984e3bdd12283713fa33c
Lesley <lesley.gover.20@ucl.ac.uk>
2020-12-16 17:00:30 +0000

    add

--- week09/improvement/boids.py
+++ week09/improvement/boids.py
@@ -5,7 +5,7 @@ for use as an exercise on refactoring.
 
 This code simulates the swarming behaviour of bird-like objects ("boids").
 """
-
+#
 from matplotlib import pyplot as plt
 from matplotlib import animation
 
